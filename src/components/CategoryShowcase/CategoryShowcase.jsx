6e210f7b9b73300c80fd0f75f5fdb5315cb1466a
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2026-09-21 17:07:59 +0300

    new

--- src/components/CategoryShowcase/CategoryShowcase.jsx
+++ src/components/CategoryShowcase/CategoryShowcase.jsx
@@ -1,44 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { getFeaturedCategories } from '../../services/categoriesService';
-// import './CategoryShowcase.css';
-
-// const CategoryShowcase = () => {
-//   const [categories, setCategories] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchCats = async () => {
-//       const data = await getFeaturedCategories();
-//       setCategories(data);
-//       setLoading(false);
-//     };
-//     fetchCats();
-//   }, []);
-
-//   if (loading) return <div className="loading">Loading categories...</div>;
-
-//   return (
-//     <section className="category-showcase">
-//       {/* <h2 className="section-title">SHOP BY CATEGORY</h2> */}
-//       <div className="category-grid">
-//         {categories.map(cat => (
-//           <Link to={`/categories/${cat.slug}`} key={cat.id} className="category-card">
-//             <img src={cat.image_url} alt={cat.name} className="category-card__img" />
-//             <div className="category-card__info">
-//               <h3>{cat.name}</h3>
-//               <p>{cat.puffs}</p>
-//               <span className="category-card__link">Explore →</span>
-//             </div>
-//           </Link>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default CategoryShowcase;
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getFeaturedCategories } from "../../services/categoriesService";
@@ -80,7 +39,7 @@ const CategoryShowcase = () => {
                 viewport={{ once: true, amount: 0.2 }}
                 transition={{ duration: 0.6 }}
               >
-                Discover Our Products
+                Discover Our Categories
               </motion.h2>
       <div className="category-grid">
         {categories.map((cat, index) => (
