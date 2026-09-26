06a3b6e914fb6d6b8ae0835d5d9f062abef15d24
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2026-09-26 10:39:28 +0300

    test versel

--- src/components/CategoryShowcase/CategoryShowcase.jsx
+++ src/components/CategoryShowcase/CategoryShowcase.jsx
@@ -39,7 +39,7 @@ const CategoryShowcase = () => {
                 viewport={{ once: true, amount: 0.2 }}
                 transition={{ duration: 0.6 }}
               >
-                Discover Our 
+                Discover Our Category
               </motion.h2>
       <div className="category-grid">
         {categories.map((cat, index) => (
